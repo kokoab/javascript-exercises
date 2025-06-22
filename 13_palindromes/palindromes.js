7de4839aaeaeb65f0d7c77a8c6ced4e4e95ebe6a
kokoab <batiancelafrancis747@gmail.com>
2025-06-22 11:42:37 +0800

    finished 12 and 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
+// palindromes.js
+function palindromes(str) {
+  // Remove all non-alphanumeric characters and convert to lowercase
+  const cleaned = str
+    .toLowerCase()
+    .replace(/[^a-z0-9]/g, '');
 
-};
+  // Check if the cleaned string is a palindrome
+  return cleaned === cleaned.split('').reverse().join('');
+}
 
-// Do not edit below this line
 module.exports = palindromes;
